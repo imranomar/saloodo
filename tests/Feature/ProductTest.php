d5c321ca7291ac15cdec99d37d81e84d1d0f6bd5
Imran Omar Bukhsh <imranomar@gmail.com>
2019-08-06 06:32:22 +0400

    new unitests: add product, add product with bundle

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -26,11 +26,65 @@ class ProductTest extends TestCase
      *
      * @return void
      */
-    public function test_can_show_post()
+    public function test_can_create_and_view_product()
     {
+        //create dummy product
         $product = factory(Product::class)->create();
-        $this->get(route('product.index', $product->id))->assertStatus(Cts::HTTP_STATUS_OK);
+
+        $this->json('GET', route('product.show', $product->id))->assertJson([
+            'id' => $product->id,
+            'title' => $product->title,
+            'price' => $product->price,
+            'discount' =>$product->discount,
+            'price_without_discount' => $product->price_without_discount,
+            'price_with_symbol' => $product->price_with_symbol,
+            'bundle' => [],
+        ]);
     }
+
+    public function test_can_create_and_view_product_with_bundle()
+    {
+        //create dummy products
+        $product_1 = factory(Product::class)->create();
+        $product_2 = factory(Product::class)->create();
+        $product_3 = factory(Product::class)->create();
+
+
+
+
+        //product 2 and 3 are bundles with 1
+        $product_1->bundle()->sync([$product_2->id,$product_3->id]);
+
+        $this->json('GET', route('product.show', $product_1->id))->assertJson([
+            'id' => $product_1->id,
+            'title' => $product_1->title,
+            'price' => $product_1->price,
+            'discount' =>$product_1->discount,
+            'price_without_discount' => $product_1->price_without_discount,
+            'price_with_symbol' => $product_1->price_with_symbol,
+            'bundle' =>
+            [
+                [
+                'id'=>$product_2->id,
+                'title'=>$product_2->title,
+                'price'=>$product_2->price,
+                'discount'=>$product_2->discount,
+                'price_without_discount'=>$product_2->price_without_discount,
+                'price_with_symbol'=>$product_2->price_with_symbol,
+                ],
+                [
+                    'id'=>$product_3->id,
+                    'title'=>$product_3->title,
+                    'price'=>$product_3->price,
+                    'discount'=>$product_3->discount,
+                    'price_without_discount'=>$product_3->price_without_discount,
+                    'price_with_symbol'=>$product_3->price_with_symbol,
+                ],
+            ],
+        ]);
+    }
+
+
 
     /**
      * Test product deletion
